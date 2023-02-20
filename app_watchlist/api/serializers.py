240c5a079df5a112949b122197cffe628b37e2a1
unknown <ragunayon@gmail.com>
2023-02-20 14:36:57 +0800

    GenerericAPIView with Mixin

--- app_watchlist/api/serializers.py
+++ app_watchlist/api/serializers.py
@@ -19,6 +19,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = "__all__"
+        lookup_field = "id"
 
 class WatchListSerializer(serializers.ModelSerializer):
     
--- app_watchlist/api/urls.py
+++ app_watchlist/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 # from app_watchlist.api.views import movie_list, movie_details
-from app_watchlist.api.views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlaformDetailAV
+from app_watchlist.api.views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlaformDetailAV, ReviewList, ReviewDetail
 
  
 urlpatterns = [
@@ -12,5 +12,8 @@ urlpatterns = [
     path('list/', WatchListAV.as_view(), name="movie-list"),
     path('<int:id>', WatchListDetailAV.as_view(), name="movie-details"),
     path('stream/', StreamPlatformAV.as_view(), name="stream"),
-    path('stream/<int:id>', StreamPlaformDetailAV.as_view(), name="stream-details")
+    path('stream/<int:id>', StreamPlaformDetailAV.as_view(), name="stream-details"),
+    path('review/', ReviewList.as_view(), name="review-list"),
+    #take note this, I used PK since we are using mixin, and by default, ID is PK
+    path('review/<int:pk>', ReviewDetail.as_view(), name="review-detail")
 ]
